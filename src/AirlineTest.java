5c5e3aaa971c4db879d1870e51f3b545cf0ca42e
Pana Alexandra Beatrice <alexandra_beatrice.pana@aut.pub.ro>
2016-05-11 00:58:34 +0300

    Ultima modificare
    Am facut si testul de care uitasem mai devreme sa il modific si avea
    eroare....alaAirline Test  , acum merge :D

--- src/AirlineTest.java
+++ src/AirlineTest.java
@@ -6,7 +6,10 @@ public class AirlineTest extends Airline {
 
 	@Test
 	public void test() {
-		Airline a[] = { new Airline("BlueAir","0123","Emma","Elicopter",320) , new Airline("BlueAir","3210","Marius","Elicopter",130) , new Airline("BlueAir","4563","Alin","Elicopter",56) , new Airline("BlueAir","9854","Alex","Elicopter",2)} ;
+		Airline a[] = { new Airline("BlueAir","0123","Emma","Elicopter",320, new Route()) ,
+				        new Airline("BlueAir","3210","Marius","Elicopter",130, new Route()) ,
+				        new Airline("BlueAir","4563","Alin","Elicopter",56, new Route()),
+				        new Airline("BlueAir","9854","Alex","Elicopter",2 ,new Route())} ;
         Airline.sortare(a); 
         for(int i=0;i<4;i++)
     			a[i].Afisare();
